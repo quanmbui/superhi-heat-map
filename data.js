3327823b090085a08e5a777efaac90784e96b036
quanmbui <quanmbui7@gmail.com>
2020-05-19 18:00:18 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,82 @@
+let data = [
+  {
+    city: 'London',
+    country: 'United Kingdom',
+    months: [5, 7, 9, 11, 14, 16, 19, 19, 17, 13, 10, 7]
+  },
+  {
+    city: 'New York',
+    country: 'United States',
+    months: [2, 2, 4, 11, 16, 22, 25, 24, 20, 14, 9, 9]
+  },
+  {
+    city: 'Sydney',
+    country: 'Australia',
+    months: [23, 23, 22, 19, 16, 14, 13, 14, 16, 18, 20, 22]
+  },
+  {
+    city: 'Reykjavik',
+    country: 'Iceland',
+    months: [0, 0, 0, 3, 6, 9, 11, 11, 7, 4, 3, 2]
+  },
+  {
+    city: 'Nuuk',
+    country: 'Greenland',
+    months: [-8, -9, -8, -5, 2, 6, 8, 6, 4, -1, -4, -6]
+  },
+  {
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+    months: [19, 20, 23, 26, 31, 33, 35, 36, 33, 29, 25, 21]
+  },
+  {
+    city: 'Honolulu',
+    country: 'United States',
+    months: [23, 23, 24, 24, 25, 26, 26, 27, 27, 27, 25, 24]
+  },
+  {
+    city: 'Oslo',
+    country: 'Norway',
+    months: [-3, -3, 2, 5, 12, 16, 18, 16, 12, 7, 2, -3]
+  },
+  {
+    city: 'Auckland',
+    country: 'New Zealand',
+    months: [20, 20, 19, 17, 14, 12, 11, 12, 13, 15, 16, 18]
+  },
+  {
+    city: 'Chicago',
+    country: 'United States',
+    months: [-6, -4, 3, 10, 15, 21, 23, 22, 18, 12, 5, -3]
+  },
+  {
+    city: 'Baltimore',
+    country: 'United States',
+    months: [3, 4, 9, 15, 20, 25, 28, 27, 23, 16, 11, 5]
+  },
+  {
+    city: 'Rome',
+    country: 'Italy',
+    months: [8, 9, 12, 14, 21, 23, 26, 24, 22, 18, 13, 10]
+  },
+  {
+    city: 'Reykjavik',
+    country: 'Iceland',
+    months: [0, 0, 0, 3, 6, 9, 11, 11, 7, 4, 3, 2]
+  },
+  {
+    city: 'Saigon',
+    country: 'Vietnam',
+    months: [27, 28, 29, 30, 30, 29, 28, 29, 28, 28, 27, 27]
+  },
+  {
+    city: 'Los Angeles',
+    country: 'United States',
+    months: [13, 13, 14, 16, 17, 19, 22, 22, 21, 18, 17, 14]
+  },
+  {
+    city: 'Tulum',
+    country: 'Mexico',
+    months: [24, 24, 25, 26, 28, 28, 28, 28, 28, 27, 26, 24]
+  }
+]
